2c9f75621e91ca4c093a0e22cdf61d90d123595d
somesh patil <someshpatil1155@gmail.com>
2026-07-06 17:30:55 +0530

    Somesh

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,12 +28,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
    // * Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+     //baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'http://localhost:100',
     headless: true,
-    screenshot: 'always',
+    screenshot: 'on',
     //video: 'on', // ✅ record for all tests
     trace: 'on-first-retry'
  
